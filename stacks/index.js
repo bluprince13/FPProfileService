f0c30683ef06d5e5d904ef9fb6c339ed117c36a4
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2021-12-28 21:47:22 +0000

    Add storage stack

--- stacks/index.js
+++ stacks/index.js
@@ -1,12 +1,14 @@
 import MyStack from "./MyStack";
+import StorageStack from "./StorageStack";
 
 export default function main(app) {
-  // Set default runtime for all functions
-  app.setDefaultFunctionProps({
-    runtime: "go1.x"
-  });
+    // Set default runtime for all functions
+    app.setDefaultFunctionProps({
+        runtime: "go1.x",
+    });
 
-  new MyStack(app, "my-stack");
+    new MyStack(app, "my-stack");
+    new StorageStack(app, "storage");
 
-  // Add more stacks
+    // Add more stacks
 }
